aa36cc48093193158a75258b813a8615314138b4
RotcivL <victor.lee.98.vic@gmail.com>
2024-01-16 17:58:36 +1100

    add setup file from fa21 starter

--- setup.py
+++ setup.py
@@ -7,6 +7,25 @@ def main():
     # Use the setup function we imported and set up the modules.
     # You may find this reference helpful: https://docs.python.org/3.6/extending/building.html
     # TODO: YOUR CODE HERE
-
+    setup(name="numc",
+        version="0.0.1",
+        description="numc matrix operations",
+        ext_modules=[
+        Extension("numc",
+                    sources=["numc.c", "matrix.c"],
+                    extra_compile_args=CFLAGS,
+                    extra_link_args=LDFLAGS,
+                    language='c')
+        ])
+    setup(name="dumbc",
+            version="0.0.1",
+            description="numc_naive matrix operations",
+            ext_modules=[
+            Extension("dumbc",
+                        sources=["numc.c", "matrix_naive.c"],
+                        extra_compile_args=CFLAGS,
+                        extra_link_args=LDFLAGS,
+                        language='c')
+            ])
 if __name__ == "__main__":
     main()
